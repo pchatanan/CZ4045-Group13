9ee8231d62834a72d165d86b56ddce8f5bb93819
Potcharaphol Chat-anan <p.chatanan@gmail.com>
2018-09-16 15:47:27 +0800

    done data analysis

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,10 +4,28 @@ import matplotlib.pyplot as plt
 import nltk
 from nltk.stem import PorterStemmer
 import random
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+import multiprocessing
+import sys
+import string
+import pickle
 
-stop_words_list = ['.', 'the', 'I', 'and', ',', 'it', 'to', 'a', '!', 'my', 'for', 'is', 'in', 'one', 'have', 'of',
-                   'this', 'on', 'you', 'was', 'with', 'so', 'It', 'n\'t', 'that', 'they', 'at', 'but', 'because',
-                   'just', 'as', 'This', 'two', 'or', 'can', 'are']
+ps = PorterStemmer()  # This is for nltk stemming
+stop_words_custom = ['n\'t', 'one', 'two', '\'s', 'would', 'get', 'very']
+stop_words_list = stopwords.words("english") + list(string.punctuation) + stop_words_custom
+
+do_not_stem_list = ["the", "this", "was", "battery", "charge", "because", "very", "verify"]
+my_stem_ref = {
+    "charged": "charge",
+    "charges": "charge",
+    "charger": "charge",
+    "charging": "charge",
+    "batteries": "battery",
+    "batterys": "battery",
+    "verified": "verify",
+    "verifies": "verify"
+}
 
 
 def check_corpus(corpus_name):
@@ -22,6 +40,20 @@ def check_corpus(corpus_name):
     except LookupError:
         print("{} is not installed. Trying to download now...".format(corpus_name))
         nltk.download(corpus_name)
+
+
+def my_stem(word):
+    """
+    This function perform stemming
+    :param word: this non-stemmed word
+    :return: stemmed word
+    """
+    if word in do_not_stem_list:
+        return word
+    if word in my_stem_ref:
+        return my_stem_ref[word]
+    else:
+        return ps.stem(word)
 
 
 def find_most_frequent(data_list, key, top_n):
@@ -78,6 +110,7 @@ def plot_frequency(result,
             ax.text(i, v, str(v) + "\n", color='blue', va='center', fontweight='bold')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.savefig('./output/{}.png'.format(title))
 
 
 def remove_stop_words(counted_list):
@@ -86,66 +119,99 @@ def remove_stop_words(counted_list):
     :param counted_list: A list of tuple containing (word, count)
     :return: the counted_list with stop words removed
     """
-    cleaned_counted_list = [item for item in counted_list if not (item[0] in stop_words_list)]
+    cleaned_counted_list = [item for item in counted_list if not (item[0].lower() in stop_words_list)]
     return cleaned_counted_list
 
 
-def main():
+def process_review(text):
+    review = json.loads(text)
+    # remove unwanted attr
+    del review['unixReviewTime']
+    del review['reviewTime']
+    sentence_tokenize_list = sent_tokenize(review["reviewText"])
+    del review['reviewText']
+    # Store result back into review
+    review["sentence_tokenize"] = sentence_tokenize_list
+    review["num_sentences"] = str(len(sentence_tokenize_list))
+    # Word Tokenize and Stemming
+    stemmed_words = []
+    non_stemmed_words = []
+    for sentence in sentence_tokenize_list:
+        word_list = word_tokenize(sentence)
+        non_stemmed_words += word_list
+        for word in word_list:
+            stemmed_words.append(my_stem(word.lower()))
+    review["non_stemmed_words"] = non_stemmed_words
+    review["num_non_stemmed_words"] = str(len(set(non_stemmed_words)))  # Set is needed (unrepeated count)
+    review["stemmed_words"] = stemmed_words
+    review["num_stemmed_words"] = str(len(set(stemmed_words)))  # Set is needed (unrepeated count)
+    return review
+
+
+def log_result(retrieval):
+    results.append(retrieval)
+    all_non_stemmed_words.extend(retrieval["non_stemmed_words"])
+    all_stemmed_words.extend(retrieval["stemmed_words"])
+    sys.stderr.write('\rdone {0:%}'.format(len(results) / n_reviews))
+
+
+if __name__ == "__main__":
+    # Load necessary corpus
+    for corpus in ["stopwords", "punkt", "averaged_perceptron_tagger"]:
+        check_corpus(corpus)
     # Load data
-    data = []
-    with open('./raw/SampleReview.json') as file:
-        for line in file:
-            data.append(json.loads(line))
+    print("Loading pickle...")
+    try:
+        results, all_non_stemmed_words, all_stemmed_words = pickle.load(open("./raw/save.p", "rb"))
+        print("Found pickle!")
+    except FileNotFoundError:
+        print("Data is not processed yet. Processing now...")
+        data = []
+        # file_name = './raw/SampleReview.json'
+        file_name = './raw/CellPhoneReview.json'
+        with open(file_name) as file:
+            for line in file:
+                data.append(line)
+        # Sentence Segmentation
+        n_reviews = len(data)
+        # Declare golbal variable to store result
+        results = []
+        # Variable for word tokenization
+        all_non_stemmed_words = []
+        all_stemmed_words = []
 
-    # Dataset Analysis
-    # Popular Products and Frequent Reviewers
-    top_10_products = find_most_frequent(data, "asin", 10)
-    top_10_reviewers = find_most_frequent(data, "reviewerID", 10)
+        # perform parallel processing (must faster than serial)
+        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
+        for item in data:
+            pool.apply_async(process_review, args=[item], callback=log_result)
+        pool.close()
+        pool.join()
+        print("")  # print linebreak
 
-    # Sentence Segmentation
-    check_corpus("punkt")
-    ps = PorterStemmer()
+        # save in pickle
+        pickle.dump((results, all_non_stemmed_words, all_stemmed_words), open("./raw/save.p", "wb"))
 
-    # Variable for word tokenization
-    all_non_stemmed_words = []
-    all_stemmed_words = []
+    # Data set Analysis
+    top_10_products = find_most_frequent(results, "asin", 10)
+    top_10_reviewers = find_most_frequent(results, "reviewerID", 10)
+    top_10_num_sentences = find_most_frequent(results, "num_sentences", 10)
+    top_10_num_non_stemmed_words = find_most_frequent(results, "num_non_stemmed_words", 10)
+    top_10_num_stemmed_words = find_most_frequent(results, "num_stemmed_words", 10)
 
-    n_reviews = len(data)
-    for i, review in enumerate(data):
-        print("processing {}/{}".format(i, n_reviews))
-        sentence_tokenize_list = nltk.tokenize.sent_tokenize(review["reviewText"])
-        # Store result back into review
-        review["sentence_tokenize"] = sentence_tokenize_list
-        review["num_sentences"] = str(len(sentence_tokenize_list))
-        # Word Tokenize and Stemming
-        stemmed_words = []
-        non_stemmed_words = []
-        for sentence in sentence_tokenize_list:
-            word_list = nltk.tokenize.word_tokenize(sentence)
-            non_stemmed_words += word_list
-            for word in word_list:
-                stemmed_words.append(ps.stem(word))
-        review["non_stemmed_words"] = non_stemmed_words
-        review["num_non_stemmed_words"] = str(len(set(non_stemmed_words)))  # Set is needed (unrepeated count)
-        review["stemmed_words"] = stemmed_words
-        review["num_stemmed_words"] = str(len(set(stemmed_words)))  # Set is needed (unrepeated count)
-        # Store all words in arrays (both stemmed and non-stemmed)
-        all_non_stemmed_words += non_stemmed_words
-        all_stemmed_words += stemmed_words
-
-    top_10_num_sentences = find_most_frequent(data, "num_sentences", 10)
-
-    random.seed(0)
+    # Print 5 sample sentences (result from sentence segmentation)
+    random.seed(0)  # seed to make random the same for consistency
     n_samples = 5
-    print("{} sampled tokenized sentences".format(n_samples))
-    for i in range(5):
-        sample_review = random.choice(data)
+    print("\n{} sampled sentences".format(n_samples))
+    i = 0
+    while i < 5:
+        sample_review = random.choice(results)
         sample_sentence_list = sample_review["sentence_tokenize"]
         sample_sentence = random.choice(sample_sentence_list)
-        print(sample_sentence)
-
-    top_10_num_non_stemmed_words = find_most_frequent(data, "num_non_stemmed_words", 10)
-    top_10_num_stemmed_words = find_most_frequent(data, "num_stemmed_words", 10)
+        if sample_sentence in stop_words_list:  # sometimes sentence segmentation break "!" into a sentence
+            i -= 1
+        else:
+            i += 1
+            print(sample_sentence)
 
     # Count words
     non_stemmed_words_frequency = Counter(all_non_stemmed_words).most_common()
@@ -155,23 +221,22 @@ def main():
     non_stemmed_words_frequency = remove_stop_words(non_stemmed_words_frequency)
     stemmed_words_frequency = remove_stop_words(stemmed_words_frequency)
 
-    print("List the top-20 most frequent words")
-    print("non_stemmed_words_frequency")
+    print("\nList the top-20 most frequent words")
+    print("\nnon_stemmed_words_frequency")
     for i in range(10):
         print(non_stemmed_words_frequency[i])
-    print("stemmed_words_frequency")
+    print("\nstemmed_words_frequency")
     for i in range(10):
         print(stemmed_words_frequency[i])
 
     # POS Tagging
-    check_corpus("averaged_perceptron_tagger")
     n_samples = 5
-    print("{} POS Tagging".format(n_samples))
+    print("\n{} POS Tagging".format(n_samples))
     for i in range(5):
-        sample_review = random.choice(data)
+        sample_review = random.choice(results)
         sample_sentence_list = sample_review["sentence_tokenize"]
         sample_sentence = random.choice(sample_sentence_list)
-        word_tokens = nltk.tokenize.word_tokenize(sample_sentence)
+        word_tokens = word_tokenize(sample_sentence)
         print(nltk.pos_tag(word_tokens))
 
     # Plotting results
@@ -186,23 +251,19 @@ def main():
                    "reviewer ID",
                    "v")
     plot_frequency(top_10_num_sentences,
-                   "Top 10 No. Sentences",
+                   "Top 10 Number Sentences",
                    "No. of sentences",
                    "No. of reviews",
                    "h")
     plot_frequency(top_10_num_non_stemmed_words,
-                   "Top 10 No. of Non-Stemmed Words (unrepeated)",
+                   "Top 10 Number of Non-Stemmed Words (unrepeated)",
                    "No. of words",
                    "No. of reviews",
                    "h")
 
     plot_frequency(top_10_num_stemmed_words,
-                   "Top 10 No. of Stemmed Words (unrepeated)",
+                   "Top 10 Number of Stemmed Words (unrepeated)",
                    "No. of words",
                    "No. of reviews",
                    "h")
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
