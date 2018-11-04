3ec56a747eb856d8563e18d2f8187c32c8f89b10
yeong jinzhi <yeong_jz@hotmail.com>
2018-11-04 21:52:21 +0800

    changed app to randomly sample reviews from original json file

--- src/comparativeExpr.py
+++ src/comparativeExpr.py
@@ -5,10 +5,11 @@ import multiprocessing
 import json
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk import pos_tag
+import random
+import time
 
 def search_comparative(text):
-    #data = json.loads(text)
-    tokenized_sentences = sent_tokenize(text)
+    tokenized_sentences = sent_tokenize(text["reviewText"])
     comparative_words = []
     for sentence in tokenized_sentences:
         word_tokens = word_tokenize(sentence)
@@ -22,28 +23,49 @@ def store_result(result):
     for i in result:
         if i not in comparative:
             comparative.append(i)
-    
-if __name__ == "__main__":
-    comparative = []
-    print("This application attempts to identify comparative words in your corpus.")
-    print("Enter/Paste your content. Ctrl-D or Ctrl-Z (Windows) followed by and Enter to register your input.")
-    contents = []
-    while True:
-        try:
-            line = input()
-        except EOFError:
-            break
-        contents.append(line)
-    print(contents)
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
-    for i in contents:
-        pool.apply_async(search_comparative, args=[i],callback=store_result)
-    pool.close()
-    pool.join()
+
+def print_results(sample):
+    print("Review analysed : ", sample["reviewText"])
     print("Comparative words found :", len(comparative))
     print("The words are : ", end="")
+    if len(comparative) == 0:
+        print("No words were found.")
+        print("\n")
+        return
     for i in range(0, len(comparative)):
         if i != (len(comparative)-1):
             print(comparative[i]+", ", end="")
         else:
             print(comparative[i])
+            print("\n")
+    
+if __name__ == "__main__":
+    
+    print("This application attempts to identify comparative words in reviews from CellPhoneReview.json.")
+##    print("Enter/Paste your content. Ctrl-D or Ctrl-Z (Windows) followed by an Enter to register your input.")
+##    contents = []
+##    while True:
+##        try:
+##            line = input()
+##        except EOFError:
+##            break
+##        contents.append(line)
+##    print(contents)
+    file = open('./raw/CellPhoneReview.json')
+    lines = file.readlines()
+    random.seed(int(time.time()))
+    print("Picking 5 random samples.")
+    print("\n")
+    for i in range(0,5):
+        comparative = []
+        sample = json.loads(random.choice(lines))
+        store_result(search_comparative(sample))
+        print("Sample {}.".format(i+1))
+        print_results(sample)
+        
+##    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)      
+##    for i in contents:
+##        pool.apply_async(search_comparative, args=[i],callback=store_result)
+##    pool.close()
+##    pool.join()
+    
